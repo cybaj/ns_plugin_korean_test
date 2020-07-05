a4d0f91e0fdc1d0fe2004bb4138bf3fb09455a2f
cybaj <cybaj@naver.com>
2020-07-05 18:05:46 +0900

    app ref

---
+++ references.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+/// <reference path="./src/platforms/android/typings/korean.d.ts" />
+/// <reference path="./src/platforms/android/typings/korean-declarations.d.ts" />
+
